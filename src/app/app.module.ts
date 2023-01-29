25e6371c6ba3525fa1fc56e9253a71b3a2011354
KRISHNAPRASADEK <kudu380013@gmail.com>
2023-01-29 18:41:27 +0530

    checkbox

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent],
-  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule,FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,11 @@
-import { group } from '@angular/animations';
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  FormControl,
+  Validators,
+  FormArray,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -8,21 +13,60 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
-  userForm: FormGroup;
   listData: any[] = [];
+  gender: boolean = false;
+  target: boolean = false;
+  skillsSet: any = [
+    { id: 1, name: 'React JS' },
 
-  constructor(private fb: FormBuilder) {
-    this.userForm = this.fb.group({
-      name: ['', Validators.required],
-      email: ['', Validators.required],
-      mobile: ['', Validators.required],
-      company: ['', Validators.required],
-      title: ['', Validators.required],
-      group: ['', Validators.required],
-    });
+    { id: 2, name: 'Node JS' },
+
+    { id: 3, name: 'Angular JS' },
+  ];
+
+  // user form group
+  userForm = this.fb.group({
+    // array
+    name: ['', [Validators.required, Validators.pattern('[a-zA-Z]*')]],
+    email: ['', [Validators.email, Validators.required]],
+    mobile: [
+      '',
+      [
+        Validators.required,
+        Validators.minLength(10),
+        Validators.pattern('[0-9]*'),
+      ],
+    ],
+    group: ['', Validators.required],
+    gender: ['', Validators.required],
+    skills: this.fb.array([], [Validators.required]),
+  });
+  selectedCheckBoxList: any = [];
+
+  constructor(private fb: FormBuilder) {}
+
+  controlOnChange(e: any) {
+    const skills: FormArray = this.userForm.get('skills') as FormArray;
+
+    if (e.target.checked) {
+      skills.push(new FormControl(e.target.value));
+      this.selectedCheckBoxList.push(e.target.value);
+    } else {
+      const index = skills.controls.findIndex(
+        (skill) => skill.value === e.target.value
+      );
+      skills.removeAt(index);
+    }
   }
-  addItem() {
-    this.listData.push(this.userForm.value);
-    this.userForm.reset();
+
+  submitForm() {
+    if (this.userForm.valid && this.userForm.value.gender != '') {
+      this.listData.push(this.userForm.value);
+      this.userForm.reset();
+    } else {
+      this.gender = true;
+      alert('Invalid Inputs');
+    }
+    console.log(this.listData);
   }
 }
